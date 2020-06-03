e3b9a31a1673c5ea726746414dbfcd1f8d5aa87f
jaimehsn <jaimehsn@gmail.com>
2020-06-03 11:17:17 +0200

    Correción en la implementación del protocolo SSL.

--- server.js
+++ server.js
@@ -2,8 +2,14 @@
 
 var express = require('express');
 var serveStatic = require('serve-static');
+var fs = require('fs')
+var https = require('https')
 var app = express();
 app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started '+ port);
+https.createServer({
+    key: fs.readFileSync('./ssl/key.pem'),
+    cert: fs.readFileSync('./ssl/cert.pem'),
+    passphrase: 'L0sm0nt3sn0t13n3n0j0s'
+}, app).listen(port);
+console.log('server started ' + port);
